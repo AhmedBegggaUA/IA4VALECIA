adfbb8b353435bec10ed040ca1b357215d981c7e
Ahmed Begga <abh28@alu.ua.es>
2023-03-27 15:16:45 +0200

    Added prescriptor results (socio-economics

--- Pagina.py
+++ Pagina.py
@@ -826,18 +826,18 @@ try:
             prescription_index = index
             country_name = country_pres
             #C1M_School closing,C2M_Workplace closing,C3M_Cancel public events,C4M_Restrictions on gatherings,C5M_Close public transport,C6M_Stay at home requirements,C7M_Restrictions on internal movement,C8EV_International travel controls,H1_Public information campaigns,H2_Testing policy,H3_Contact tracing,H6M_Facial Coverings
-            NPI_COLUMNS = ['C1_School closing',
-                'C2_Workplace closing',
-                'C3_Cancel public events',
-                'C4_Restrictions on gatherings',
-                'C5_Close public transport',
-                'C6_Stay at home requirements',
-                'C7_Restrictions on internal movement',
-                'C8_International travel controls',
+            NPI_COLUMNS = ['C1M_School closing',
+                'C2M_Workplace closing',
+                'C3M_Cancel public events',
+                'C4M_Restrictions on gatherings',
+                'C5M_Close public transport',
+                'C6M_Stay at home requirements',
+                'C7M_Restrictions on internal movement',
+                'C8EV_International travel controls',
                 'H1_Public information campaigns',
                 'H2_Testing policy',
                 'H3_Contact tracing',
-                'H6_Facial Coverings']
+                'H6M_Facial Coverings']
             region_name = None
             pdf = prescriptions
             #if not reg_pres:
