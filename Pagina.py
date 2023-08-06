69aaf9397ba4fa6b7c2b6c3c16d6742687364acc
Ahmed Begga <abh28@alu.ua.es>
2023-08-06 11:14:24 +0200

    Some warnings

--- Pagina.py
+++ Pagina.py
@@ -784,7 +784,7 @@ SUPERA COVID-19 Santander-CRUE (CD4COVID19 2020–2021), Fundación BBVA for SAR
             country_name = country_pres
             #if not reg_pres:
             #    stringency = stringency[(stringency.RegionName == region)]
-            cdf = stringency[(stringency['PrescriptorName'] == 'FeatGreedy') & (stringency.CountryName == country_pres)]
+            cdf = stringency[(stringency['PrescriptorName'] == 'V4C') & (stringency.CountryName == country_pres)]
             st.write(cdf)
             #Plotly
             fig = go.Figure()
